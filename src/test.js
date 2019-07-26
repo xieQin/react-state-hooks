3d6a75363a0dbe2c1bbd9a8f7230475261f8e5d7
cherish <xieqin1991@gmail.com>
2019-07-26 14:01:51 +0800

    Add Component

---
+++ src/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Test extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return (
+      <div>1111</div>
+    )
+  }
+}
+
+export default Test
