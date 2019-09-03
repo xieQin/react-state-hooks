b9087e22abd815f4a71921a05231bbb56e856164
cherish <xieqin1991@gmail.com>
2019-09-03 11:10:51 +0800

    Add useEffect

--- src/components/example.tsx
+++ src/components/example.tsx
@@ -2,15 +2,33 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Nowtime from './nowtime'
 import { useStore } from '../models'
+import axios from 'axios'
+
+interface Iitem {
+  objectID: number,
+  url: string,
+  title: string
+}
 
 const Example = () => {
   const [count, setCount] = useState(0);
   const [nums, setNums] = useState(100);
   const [state, actions] = useStore('Test')
   console.log(state, actions, 'example')
+
+  const [data, setData] = useState({ hits: [] });
   useEffect (() => {
     document.title = `You clicked ${count} times`
   });
+
+  useEffect(async () => {
+    const result = await axios(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+
+    setData(result.data);
+  });
+
   return (
     <div>
       <Nowtime/>
@@ -18,6 +36,13 @@ const Example = () => {
       <p>You Clicked {count} times, Nums is {nums}</p>
       <button onClick={() => setCount(count + 1)}>Count ++</button>
       <button onClick={() => setNums(nums - 1)}>delete nums</button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
